a570d33bad12b80317118429c1d910e3f5de622d
Yudis <yudis10@users.noreply.github.com>
2023-11-04 12:03:48 +0700

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,28 @@
+import sitemap from "@astrojs/sitemap"
+import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config"
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://online.detik.com", //  Change to your site URL  
+  vite: {
+    build: {
+      cssCodeSplit: false,
+    },
+  },
+  integrations: [
+    sitemap(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+  base: "/design/static/starter-project/preview/", // Change to your base path
+  build: {
+    format: "file",
+    assets: "_starter-project",// Change to your project name
+    inlineStylesheets: "never",
+  },
+  compressHTML: false,
+  output: "static",
+  outDir: "./preview",
+})
